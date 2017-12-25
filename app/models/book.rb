7f99d2377f54118f3a8a7c5da391d37a15e4d83e
Rob <rob@rastating.com>
2017-12-25 15:19:31 +0000

    Add methods for calculating checksums and instantiating books

--- app/models/book.rb
+++ app/models/book.rb
@@ -17,9 +17,59 @@ module EBL
         validates_max_length 500, :rights, allow_nil: true
         validates_max_length 200, :source, allow_nil: true
         validates_max_length 10, :epub_version, allow_nil: true
+        validates_max_length 100, :checksum, allow_nil: true
 
         validates_presence :description
         validates_presence :drm_protected
+        validates_presence :path
+      end
+
+      # @return [String] the name of the first author, or "Unknown"
+      def primary_author
+        return 'Unknown' if authors.empty? || authors[0].name.empty?
+        authors[0].name
+      end
+
+      # Recalculate the checksum of the file stored at #path.
+      def refresh_checksum
+        if path.nil? || path.empty? || !File.exist?(path)
+          self.checksum = ''
+          return
+        end
+
+        data = File.read(path)
+        md5 = Digest::MD5.new
+        md5 << data
+        self.checksum = md5.hexdigest
+      end
+
+      # Set the path value and refresh the checksum value.
+      # @param path [String] the path to the ePub.
+      # @param should_save [Boolean] flag indicating whether to also
+      #   make a call to #save.
+      def update_path_and_refresh_checksum(path, should_save = false)
+        self.path = path
+        refresh_checksum
+        save if should_save
+      end
+
+      # Parse an ePub file into an {EBL::Models::Book}.
+      # @param path [String] the path to the ePub file to parse.
+      # @return [EBL::Models::Book] a book model based on the
+      #   metadata of an ePub file.
+      def self.from_epub(path)
+        epub = EPUBInfo.get(path)
+        book = EBL::Models::Book.new
+        book.title = epub.titles[0]
+
+        book.description = 'This book has no description'
+        if !epub.description.nil? && !epub.description.empty?
+          book.description = epub.description
+        end
+
+        book.drm_protected = epub.drm_protected
+        book.update_path_and_refresh_checksum(path)
+        book
       end
     end
   end
--- spec/app/models/book_spec.rb
+++ spec/app/models/book_spec.rb
@@ -1,6 +1,26 @@
 require_relative '../../spec_helper'
 
 describe EBL::Models::Book, type: :model do
+  let(:subject) { described_class.new }
+  let(:epub_dbl) do
+    double(
+      'epub',
+      titles: %w[title],
+      description: 'desc',
+      drm_protected: false
+    )
+  end
+
+  before(:each) do
+    EBL::Models::Book.create(
+      title: 'test',
+      description: 'test',
+      drm_protected: false,
+      path: '/test',
+      checksum: 'test'
+    )
+  end
+
   it { is_expected.to have_one_to_many :authors }
   it { is_expected.to have_one_to_many :subjects }
   it { is_expected.to have_one_to_many :identifiers }
@@ -10,8 +30,158 @@ describe EBL::Models::Book, type: :model do
   it { is_expected.to validate_max_length 500, :publisher, allow_nil: true }
   it { is_expected.to validate_max_length 500, :rights, allow_nil: true }
   it { is_expected.to validate_max_length 200, :source, allow_nil: true }
+  it { is_expected.to validate_max_length 100, :checksum, allow_nil: true }
+
   it { is_expected.to validate_max_length 10, :epub_version, allow_nil: true }
 
   it { is_expected.to validate_presence :description }
   it { is_expected.to validate_presence :drm_protected }
+  it { is_expected.to validate_presence :path }
+
+  describe '#refresh_checksum' do
+    context 'when #path is empty' do
+      it 'sets #checksum to a blank string' do
+        expect(subject.checksum).to be_nil
+        subject.path = ''
+        subject.refresh_checksum
+        expect(subject.checksum).to eq ''
+      end
+    end
+
+    context 'when #path is nil' do
+      it 'sets #checksum to a blank string' do
+        expect(subject.checksum).to be_nil
+        subject.path = nil
+        subject.refresh_checksum
+        expect(subject.checksum).to eq ''
+      end
+    end
+
+    context 'when the file in #path does not exist' do
+      it 'sets #checksum to a blank string' do
+        expect(subject.checksum).to be_nil
+        subject.path = '/this/file/could/not/possibly/exist.test'
+        subject.refresh_checksum
+        expect(subject.checksum).to eq ''
+      end
+    end
+
+    context 'when the file in #path exists' do
+      it 'sets #checksum to the MD5 hash of the file in #path' do
+        allow(File).to receive(:read).and_return('test')
+        allow(File).to receive(:exist?).and_return(true)
+
+        subject.path = '/test'
+        expect(subject.checksum).to be_nil
+
+        subject.refresh_checksum
+        expect(subject.checksum).to eq '098f6bcd4621d373cade4e832627b4f6'
+      end
+    end
+  end
+
+  describe '#update_path_and_refresh_checksum' do
+    context 'when path is empty' do
+      it 'sets #checksum to a blank string' do
+        expect(subject.checksum).to be_nil
+        subject.update_path_and_refresh_checksum ''
+        expect(subject.checksum).to eq ''
+      end
+    end
+
+    context 'when path is nil' do
+      it 'sets #checksum to a blank string' do
+        expect(subject.checksum).to be_nil
+        subject.update_path_and_refresh_checksum nil
+        expect(subject.checksum).to eq ''
+      end
+    end
+
+    context 'when the file in path does not exist' do
+      it 'sets #checksum to a blank string' do
+        expect(subject.checksum).to be_nil
+        subject.update_path_and_refresh_checksum '/this/will/not/exist.test'
+        expect(subject.checksum).to eq ''
+      end
+    end
+
+    context 'when the file in path exists' do
+      it 'sets #checksum to the MD5 hash of the file in path' do
+        allow(File).to receive(:read).and_return('test')
+        allow(File).to receive(:exist?).and_return(true)
+
+        expect(subject.checksum).to be_nil
+
+        subject.update_path_and_refresh_checksum '/test'
+        expect(subject.checksum).to eq '098f6bcd4621d373cade4e832627b4f6'
+      end
+    end
+
+    context 'when should_save is set to true' do
+      it 'will call #save at the end of the process' do
+        allow(File).to receive(:read).and_return('test')
+        allow(File).to receive(:exist?).and_return(true)
+
+        invoked_save = false
+        allow(subject).to receive(:save) do
+          invoked_save = true
+        end
+
+        subject.update_path_and_refresh_checksum '/test', true
+        expect(invoked_save).to be true
+      end
+    end
+  end
+
+  describe '#primary_author' do
+    context 'when no authors are associated with the book' do
+      it 'returns "Unknown"' do
+        expect(subject.authors.count).to eq 0
+        expect(subject.primary_author).to eq 'Unknown'
+      end
+    end
+
+    context 'when the author name is blank' do
+      it 'returns "Unknown"' do
+        subject = EBL::Models::Book.first
+        subject.add_author(name: '')
+        expect(subject.authors.count).to eq 1
+        expect(subject.primary_author).to eq 'Unknown'
+      end
+    end
+
+    context 'when one or more valid authors exist' do
+      it 'returns the name of the first author' do
+        subject = EBL::Models::Book.first
+        subject.add_author(name: 'Emmanuel Goldstein')
+        subject.add_author(name: 'George Orwell')
+        expect(subject.primary_author).to eq 'Emmanuel Goldstein'
+      end
+    end
+  end
+
+  describe '.from_epub' do
+    it 'creates an unsaved Book from the metadata of the file at path' do
+      allow(File).to receive(:read).and_return('test')
+      allow(File).to receive(:exist?).and_return(true)
+      allow(EPUBInfo).to receive(:get).and_return epub_dbl
+
+      subject = EBL::Models::Book.from_epub('/test')
+
+      expect(subject.title).to eq 'title'
+      expect(subject.description).to eq 'desc'
+      expect(subject.drm_protected).to be false
+      expect(subject.checksum).to eq '098f6bcd4621d373cade4e832627b4f6'
+    end
+
+    it 'sets a default #description if it is blank or nil' do
+      allow(File).to receive(:read).and_return('test')
+      allow(File).to receive(:exist?).and_return(true)
+      allow(EPUBInfo).to receive(:get).and_return epub_dbl
+      allow(epub_dbl).to receive(:description).and_return ''
+
+      subject = EBL::Models::Book.from_epub('/test')
+      expect(subject.description).to eq 'This book has no description'
+    end
+  end
 end
