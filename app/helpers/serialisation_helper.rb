153364a58447d1a31ff8f078f81eca9e65b29cbd
Rob <rob@rastating.com>
2018-01-13 23:09:35 +0000

    Add more serialisation helpers

--- app/helpers/serialisation_helper.rb
+++ app/helpers/serialisation_helper.rb
@@ -8,9 +8,58 @@ module EBL
       # @return [Hash] a hash of the author data for client consumption.
       def hashify_author(author)
         {
-          id: author.id,
-          name: author.name,
+          id:         author.id,
+          name:       author.name,
           book_count: author.books.length
+        }
+      end
+
+      # Create a hash of the book for consumption by the client application.
+      # @param book [EBL::Models::Book] the book to hashify.
+      # @return [Hash] a hash of the book data for client consumption.
+      def hashify_book(book)
+        {
+          id:            book.id,
+          title:         book.title,
+          description:   book.description,
+          publisher:     book.publisher,
+          drm_protected: book.drm_protected,
+          epub_version:  book.epub_version,
+          rights:        book.rights,
+          source:        book.source,
+          checksum:      book.checksum,
+          subjects:      book.subjects.map { |s| hashify_subject(s) },
+          identifiers:   book.identifiers.map { |i| hashify_identifier(i) },
+          dates:         book.dates.map { |d| hashify_date(d) }
+        }
+      end
+
+      # Create a hash of the subject for consumption by the client application.
+      # @param subject [EBL::Models::Subject] the subject to hashify.
+      # @return [Hash] a hash of the subject data for client consumption.
+      def hashify_subject(subject)
+        {
+          name: subject.name
+        }
+      end
+
+      # Create a hash of the identifier for consumption by the client application.
+      # @param identifier [EBL::Models::Identifier] the identifier to hashify.
+      # @return [Hash] a hash of the identifier data for client consumption.
+      def hashify_identifier(identifier)
+        {
+          identifier: identifier.identifier,
+          scheme:     identifier.scheme
+        }
+      end
+
+      # Create a hash of the date for consumption by the client application.
+      # @param date [EBL::Models::Date] the date to hashify.
+      # @return [Hash] a hash of the date data for client consumption.
+      def hashify_date(date)
+        {
+          date:  date.date.strftime('%Y-%m-%d'),
+          event: date.event
         }
       end
 
@@ -19,9 +68,9 @@ module EBL
       # @return [Hash] a hash of the user data for client consumption.
       def hashify_user(user)
         {
-          id: user.id,
+          id:       user.id,
           username: user.username,
-          locked: user.locked
+          locked:   user.locked
         }
       end
     end
--- spec/app/helpers/serialisation_helper_spec.rb
+++ spec/app/helpers/serialisation_helper_spec.rb
@@ -38,4 +38,90 @@ describe EBL::Helpers::SerialisationHelper do
       expect(subject.hashify_user(user)).to eq hashified_user
     end
   end
+
+  describe '#hashify_date' do
+    it 'returns a hash of the object' do
+      date = EBL::Models::Date.new(
+        date:  '2018-01-13',
+        event: 'test'
+      )
+
+      hashified_date = { date: '2018-01-13', event: 'test' }
+      expect(subject.hashify_date(date)).to eq hashified_date
+    end
+  end
+
+  describe '#hashify_identifier' do
+    it 'returns a hash of the identifier' do
+      identifier = EBL::Models::Identifier.new(
+        identifier: 'test',
+        scheme:     'test'
+      )
+
+      hash = { identifier: 'test', scheme: 'test' }
+      expect(subject.hashify_identifier(identifier)).to eq hash
+    end
+  end
+
+  describe '#hashify_subject' do
+    it 'returns a hash of the subject' do
+      s = EBL::Models::Subject.new(
+        name: 'test'
+      )
+
+      hash = { name: 'test' }
+      expect(subject.hashify_subject(s)).to eq hash
+    end
+  end
+
+  describe '#hashify_book' do
+    it 'returns a hash of the book' do
+      book = EBL::Models::Book.create(
+        title:         'title',
+        description:   'description',
+        publisher:     'publisher',
+        drm_protected: true,
+        epub_version:  1,
+        rights:        'rights',
+        source:        'source',
+        checksum:      'checksum',
+        path:          '/path/to/epub'
+      )
+
+      book.add_subject(EBL::Models::Subject.new(name: 'subject1'))
+      book.add_subject(EBL::Models::Subject.new(name: 'subject2'))
+
+      book.add_identifier(
+        EBL::Models::Identifier.new(identifier: 'test', scheme: 'test')
+      )
+
+      book.add_date(EBL::Models::Date.new(date: '2018-01-13', event: 'test'))
+      book.add_date(EBL::Models::Date.new(date: '2018-01-14', event: 'test'))
+
+      hash = {
+        id:            1,
+        title:         'title',
+        description:   'description',
+        publisher:     'publisher',
+        drm_protected: true,
+        epub_version:  1,
+        rights:        'rights',
+        source:        'source',
+        checksum:      'checksum',
+        subjects:      [
+          { name: 'subject1' },
+          { name: 'subject2' }
+        ],
+        identifiers:   [
+          { identifier: 'test', scheme: 'test' }
+        ],
+        dates:         [
+          { date: '2018-01-13', event: 'test' },
+          { date: '2018-01-14', event: 'test' }
+        ]
+      }
+
+      expect(subject.hashify_book(book)).to eq hash
+    end
+  end
 end
