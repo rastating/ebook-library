79bd09821cf092fcf1d5c9e8aa404a22718244f9
Rob <rob@rastating.com>
2018-06-22 21:07:59 +0100

    Update packages and add production build

---
+++ public/client/webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin();
+
+module.exports = {
+  entry: [
+    './index.jsx'
+  ],
+  output: {
+    path: path.resolve('../build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'vendor', to: 'vendor' },
+      { from: 'assets', to: 'assets' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+      hash: true,
+      title: 'E-book Library'
+    })
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/,  loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/ }
+    ]
+  }
+}
